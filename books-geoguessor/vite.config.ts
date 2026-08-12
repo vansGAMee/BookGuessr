092f198bd900ecc72cfa20bb6cfac661a439bd7f
voxelpy <121387731+voxelpy@users.noreply.github.com>
2026-08-12 16:34:48 +0400

    fix BookGuessr Vercel build

--- books-geoguessor/vite.config.ts
+++ books-geoguessor/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
